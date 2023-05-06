153e204a38e36fc4164130b3b7523a734ebfa71c
JemimaNyaguthii <jemimanyaguthiiwambugu@gmail.com>
2023-05-06 20:47:22 +0300

    arrays

--- arrays.js
+++ arrays.js
@@ -12,36 +12,42 @@ function reverse_elements(){
 //   { name: 'Charlie', age: 14 },
 //   { name: 'Max', age: 19 },
 // ];
-
- function arrayOfPeople(people){
-    return people.filter(people=>people.age>=18,
-    people=>people.name)
- }
- const people=[
+const people=[
     { name: 'Alice', age: 17 },
       { name: 'Eunice', age: 22 },
       { name: 'Charlie', age: 14 },
       { name: 'Max', age: 19 },               
 ];
-console.log(arrayOfPeople(people))
+ function arrayOfPeople(people){
+    sum=0;
+    for(let i=0;i<people.length;i++){ 
+    if(people[i].age<18){
+        sum+=people[i].age;
+      }
+    }
+     return sum;
+}
+
+console.log(arrayOfPeople(people));
 
 // Using JS functions and an array of numbers, 
 // return positive if an element within the array
 // is positive, negative if an element is negative, else zero
 function negative_nums(array_nums){
-    for (leti= 0;i < array_nums.length;i++) {
-        if (i<0) {
-            return positive
-            
-        }
-        else{
-            return positive
-        }
-    
-        
+    if(arrayOfPeople.nums>=0){
+        return "positive"
     }
-    
+    else if(arrayOfPeople.nums<0){
+        return "negative"
+    }
+    else{
+        return "zero"
+    }
 }
-let array_nums=[1,4,5,6,7,8,9];
+let array_nums=[1,2,3,4,5]
 console.log(negative_nums(array_nums))
+
+// Given an array of objects, where each object represents an employee with an id, name, and salary 
+// property, write a function that returns a new array of employee objects sorted by their 
+// salary in ascending order.
 
